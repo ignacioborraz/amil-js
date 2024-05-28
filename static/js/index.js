d3e78353dc32179709635beb60899c97134708a8
albertojoseferrari <138826102+albertojoseferrari@users.noreply.github.com>
2024-05-28 15:37:30 -0300

    Logica index (#6)
    * Se actualiza header, DJs y footer
    
    * Nuevas modificaciones

--- static/js/index.js
+++ static/js/index.js
@@ -16,3 +16,22 @@ window.onscroll = function () {
         button.style.display = "none" 
     }
 };
+
+fetch("./public/events.json")
+  .then((res) => res.json())
+  .then((res) => {
+  console.log(res);
+  let events = res
+      .map(
+        (each) => `
+        <div class="fiesta-item" id="${each.id}">
+        <a href="./details.html?id=${each.id}" >
+            <img src="${each.imagen}" alt="${each.artista}" height="224px" class="fiesta-item-img">
+            <div class="cuadrado"><span class="day">24</span><span class="month">Mayo</span></div>
+        </a>
+        </div>
+        `
+      )
+      .join("");
+    document.querySelector("#events").innerHTML = events;
+  })
